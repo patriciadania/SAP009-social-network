6c16846bbeed7bc6be378bfe17b90773a568ba2c
Ana <ana_leticia@geb.inatel.br>
2023-03-08 10:38:53 -0300

    atualizando erros

---
+++ src/servicesFirebase/firebaseAuth.js
@@ -0,0 +1,34 @@
+//import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-auth-compat.js";
+c
+// const auth = getAuth();
+
+// //cadastro de usuarios novos
+// export const createUser = () => {
+
+//     createUserWithEmailAndPassword(auth, email, password)
+//         .then((userCredential) => {
+//             // Signed in
+//             const user = userCredential.user;
+//             // ...
+//         })
+//         .catch((error) => {
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+//             // ..
+//         });
+// }
+
+
+// //login de usuarios existentes
+// export const SingIn = () => {
+//     signInWithEmailAndPassword(auth, email, password)
+//         .then((userCredential) => {
+//             // Signed in
+//             const user = userCredential.user;
+//             // ...
+//         })
+//         .catch((error) => {
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+//         });
+// }
